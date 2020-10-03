eb8d53f2a9f6b8d817086f12af84f1a4d95bf607
Chandrakanth Saravanan <chandra2791@gmail.com>
2020-10-03 16:32:01 +0000

    Added linked list file

---
+++ linked_list.py
@@ -0,0 +1,8 @@
+#Create a python menu driven program for:
+#Create a link list of 5 nodes, take data from user.
+#Insert an element in front, last and middle of the list.
+#Delete a node from front, last and middle.
+#Sort the list in ascending and descending order.
+#Search a node from a sorted and unsorted list.
+#Print the list.
+#Count the number of nodes in the list.
